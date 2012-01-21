8deb0a110f16b36119929832591b51b738a39a47
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-01-21 07:35:12 -0800

    Add ensure_mongo.

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,12 @@
-from fabric.api import *
-from cuisine import *
+from fabricant import *
+
 
 def dev():
     env.hosts = ['vbox']
-    env.type = 'development'
+    env.environment = 'development'
     env.project_dir = '/vagrant/gitorama.com'
     use_ssh_config(env)
+
 
 def create_env():
     with cd(env.project_dir):
@@ -14,10 +15,29 @@ def create_env():
         run('env/bin/pip install -r requirements.txt')
 
 
+def ensure_apt_source(source_line):
+    if not run("grep '%s' /etc/apt/sources.list >& /dev/null && echo OK ; true" % source_line).endswith('OK'):
+        sudo("echo '%s' >> /etc/apt/sources.list" % source_line)
+
+
+def ensure_mongo():
+    if not command_check('mongod'):
+        sudo('apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10')
+        ensure_apt_source('deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen')
+        sudo("apt-get --yes update")
+        package_ensure(['mongodb-10gen'])
+
+    upstart_ensure('mongodb')
+
+
 def setup():
     create_env()
+    ensure_mongo()
 
-def run_server():
+
+def runserver():
+    require('project_dir', provided_by=[dev])
+
     with cd(env.project_dir):
-        run('env/bin/python app.py')
+        local('env/bin/python app.py')
 
